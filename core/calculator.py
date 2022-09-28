33c9f3d44be88c3886a7637c62f16e48755d79c9
Michael Calloo <admin@cookienerds.com>
2022-09-28 11:33:05 +0800

    move JS functions to python

---
+++ core/calculator.py
@@ -0,0 +1,100 @@
+from __future__ import annotations
+from dataclasses import dataclass
+from enum import Enum
+from typing import Optional
+from datetime import datetime
+
+
+class ActionType(Enum):
+    ADD = 'add'
+    SUBTRACT = 'sub'
+    SET = 'set'
+
+    @staticmethod
+    def value_from(name: str) -> Optional[ActionType]:
+        return next(filter(
+            lambda i: i.value == name,
+            (i for i in ActionType)
+        ))
+
+
+@dataclass
+class Calculator:
+    initial_deposit: float = 0
+    contribution_amount: float = 0
+    investment_timespan: int = 0
+    estimated_return: float = 0
+    future_balance: float = 0
+    contribution_period: int = 12
+    compound_period: int = 12
+
+    def get_amount(self, field: str):
+        """
+        Used to retrieve values from the calculator. E.g. initial deposit
+        :param field: name of the field
+        :return:
+        """
+        return getattr(self, field)
+
+    def change_amount(self, field: str, amount: float, action_string: str):
+        """
+        Used to change a property on the calculator. E.g. initial deposit, contribution amount e.t.c
+        :param field:
+        :param amount:
+        :param action_string:
+        :return:
+        """
+        action = ActionType.value_from(action_string)
+        change = getattr(self, field)
+
+        if action == ActionType.ADD:
+            change += amount
+        elif action == ActionType.SUBTRACT:
+            change -= amount
+        elif action == ActionType.SET:
+            change = amount
+
+        if change >= 0:
+            setattr(self, field, change)
+            return change
+        return getattr(self, field)
+
+    def calculate_future_values(self):
+        """
+        Calculates the future value
+        """
+        current_year = datetime.now().year
+        labels = [year for year in range(current_year, current_year + int(self.investment_timespan))]
+        principal_dataset = {
+            "label": 'Total Principal',
+            "backgroundColor": 'rgb(0, 123, 255)',
+            "data": []
+        }
+        earnings_dataset = {
+            "label": "Total Earnings",
+            "backgroundColor": 'rgb(23, 162, 184)',
+            "data": []
+        }
+
+        for time in range(1, self.investment_timespan):
+            principal = self.initial_deposit + (self.contribution_amount * self.contribution_period * time)
+            earnings = 0
+            balance = principal
+            estimated_return = self.estimated_return / 100
+
+            if estimated_return:
+                gains = (1 + estimated_return / self.compound_period) ** (self.compound_period * time)
+                compound_earnings = self.initial_deposit + gains
+                contribution_earnings = self.contribution_amount * (gains - 1) / (self.compound_period / self.contribution_period)
+                earnings = compound_earnings + contribution_earnings - principal
+                balance = compound_earnings + contribution_earnings
+
+            self.future_balance = balance
+            principal_dataset['data'].append(round(principal, 2))
+            earnings_dataset['data'].append(round(earnings, 2))
+
+        return {
+            "labels": labels,
+            "principal_dataset": principal_dataset,
+            "earnings_dataset": earnings_dataset
+        }
--- main.py
+++ main.py
@@ -1,7 +1,144 @@
 import webview
+from webview import Window
+
+from core.calculator import Calculator
 
 
-# Press the green button in the gutter to run the script.
+def main(win: Window):
+    # still needs testing
+    # win.evaluate_js(
+    #     """
+    #     (async() => {
+    #         const ctx = document.getElementById('myChart').getContext('2d');
+    #         const investment_timespan = document.querySelector('#investment_timespan');
+    #         const investment_timespan_text = document.querySelector('#investment_timespan_text');
+    #         const future_balance = document.querySelector('#future_balance');
+    #         const depositSelector = document.querySelector('#initial_deposit');
+    #         const estimatedReturnSelector = document.querySelector('#estimated_return');
+    #         const contributionSelector = document.querySelector('#contribution_amount');
+    #
+    #         const updateChart = async () => {
+    #             const {labels, principal_dataset, earnings_dataset} = await pywebview.api.calculate_future_values();
+    #
+    #             chart.data.labels = labels;
+    #             chart.data.datasets[0].data = principal_dataset.data;
+    #             chart.data.datasets[1].data = earnings_dataset.data;
+    #             chart.update();
+    #             future_balance.innerHTML = '$' + (await pywebview.api.get_amount('future_balance')).toFixed(2);
+    #         }
+    #
+    #         const changeHandler = async (dom, action = 'set') => {
+    #             const name = dom.getAttribute('id');
+    #             const min = parseFloat(dom.getAttribute('min'));
+    #             const max = parseFloat(dom.getAttribute('max'));
+    #             const oldValue = dom.dataset.value || dom.defaultValue || 0;
+    #             let newValue = parseFloat(dom.value.replace(/\$/, ''));
+    #             const step = ['investment_timespan'].includes(name) ? newValue: parseFloat(dom.getAttribute('step')) || 1;
+    #
+    #             if (isNaN(parseFloat(newValue))) {
+    #                 newValue = oldValue;
+    #             } else {
+    #                 newValue = await pywebview.api.change_amount(name, step, action);
+    #                 newValue = newValue < min ? min : newValue > max ? max : newValue;
+    #             }
+    #
+    #             dom.dataset.value = newValue;
+    #             dom.value = (dom.dataset.prepend || '') + newValue + (dom.dataset.append || '');
+    #             await updateChart();
+    #         };
+    #
+    #         investment_timespan.value = await pywebview.api.get_amount('investment_timespan');
+    #         investment_timespan_text.innerHTML = investment_timespan.value + ' years';
+    #         depositSelector.dataset.value = await pywebview.api.get_amount('initial_deposit');
+    #         depositSelector.value = '$' + depositSelector.dataset.value;
+    #         contributionSelector.dataset.value = await pywebview.api.get_amount('contribution_amount');
+    #         contributionSelector.value = '$' + depositSelector.dataset.value;
+    #         estimatedReturnSelector.dataset.value = await pywebview.api.get_amount('estimated_return');
+    #         estimatedReturnSelector.value = estimatedReturnSelector.dataset.value + '%';
+    #
+    #         depositSelector.addEventListener('change', function(){ changeHandler(this) });
+    #         estimatedReturnSelector.addEventListener('change', function(){ changeHandler(this) });
+    #         contributionSelector.addEventListener('change', function(){ changeHandler(this) });
+    #         investment_timespan.addEventListener('change', async function () {
+    #             changeHandler(this);
+    #             investment_timespan_text.innerHTML = this.value + ' years';
+    #             await updateChart();
+    #         });
+    #         investment_timespan.addEventListener('input', function () {
+    #             investment_timespan_text.innerHTML = this.value + ' years';
+    #         });
+    #
+    #         Array.from(document.querySelectorAll('[name="contribution_period"], [name="compound_period"]'))
+    #         .forEach(radio => radio.addEventListener('change', async function() {
+    #             const name = this.getAttribute('name');
+    #             if (name.includes('compound_period')) {
+    #                 await pywebview.api.change_amount(name, parseInt(document.querySelector('[name="compound_period"]:checked').value), 'set');
+    #             } else {
+    #                 await pywebview.api.change_amount(name, parseInt(document.querySelector('[name="contribution_period"]:checked').value), 'set');
+    #             }
+    #             await updateChart();
+    #         }));
+    #
+    #         Array.from(document.querySelectorAll('[data-counter]'))
+    #         .forEach(
+    #             button => button.addEventListener('click', function () {
+    #                 var field = document.querySelector('[name="' + this.dataset.field + '"]'),
+    #                     action = this.dataset.counter;
+    #
+    #                 if (field) {
+    #                     changeHandler(field, action);
+    #                 }
+    #             })
+    #         );
+    #
+    #         const {label, ...data} = await pywebview.api.calculate_future_values();
+    #         const chart = new Chart(ctx, {
+    #             type: 'bar',
+    #             data: {label, datasets: Object.values(data)},
+    #             options: {
+    #                 legend: {
+    #                     display: false
+    #                 },
+    #                 tooltips: {
+    #                     mode: 'index',
+    #                     intersect: false,
+    #                     callbacks: {
+    #                         label: function (tooltipItem, data) {
+    #                             console.log(data.datasets[tooltipItem.datasetIndex]);
+    #                             return data.datasets[tooltipItem.datasetIndex].label + ': $' + tooltipItem.yLabel;
+    #                         }
+    #                     }
+    #                 },
+    #                 responsive: true,
+    #                 scales: {
+    #                     xAxes: [{
+    #                         stacked: true,
+    #                         scaleLabel: {
+    #                             display: true,
+    #                             labelString: 'Year'
+    #                         }
+    #                     }],
+    #                     yAxes: [{
+    #                         stacked: true,
+    #                         ticks: {
+    #                             callback: function (value) {
+    #                                 return '$' + value;
+    #                             }
+    #                         },
+    #                         scaleLabel: {
+    #                             display: true,
+    #                             labelString: 'Balance'
+    #                         }
+    #                     }]
+    #                 }
+    #             }
+    #         });
+    #     })();
+    #     """
+    # )
+    pass
+
+
 if __name__ == '__main__':
-    webview.create_window('Financial Calculator', 'web/index.html')
-    webview.start(gui='edgehtml', debug=True)
+    window = webview.create_window('Financial Calculator', 'web/index.html', js_api=Calculator())
+    webview.start(main, window, gui='cef')
