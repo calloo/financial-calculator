3adb48596128b5796117f6bd846dd1ea33ff9ea5
Michael Calloo <admin@cookienerds.com>
2022-09-29 17:40:02 +0800

    update setup.sh file

--- setup.sh
+++ setup.sh
@@ -1,4 +1,10 @@
-apt-get --yes install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0
-python3 -m venv venv
+apt-get --yes install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+
+PYENV = "venv"
+if [[ ! -e $PYENV ]]; then
+    python3 -m venv venv
+elif [[ ! -d $PYENV ]]; then
+    echo "$PYENV already exists but is not a directory"
+fi
 source venv/bin/activate
 pip install -r requirements.txt
