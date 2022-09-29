33fe33bb424f37008a9f5fe5f18be60251cb9faf
Michael Calloo <admin@cookienerds.com>
2022-09-29 18:08:05 +0800

    fix setup

--- setup.sh
+++ setup.sh
@@ -2,11 +2,11 @@
 sudo apt-get update
 sudo apt-get --yes install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
 
-DIR = "venv"
+DIR="venv"
 if [[ ! -d "$DIR" ]]; then
     echo "creating python environment - $DIR"
     python3 -m venv venv
-else; then
+else
     echo "$DIR already exists. Using as virtual environment"
 fi
 source venv/bin/activate
