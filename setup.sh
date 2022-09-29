4c6d5f62b5a7363343eec3795a096a1a7da53eb8
Michael Calloo <admin@cookienerds.com>
2022-09-29 17:45:11 +0800

    fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,11 @@
-apt-get --yes install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
+sudo apt-get --yes install libgirepository1.0-dev gcc libcairo2-dev pkg-config python3-dev gir1.2-gtk-3.0
 
 PYENV = "venv"
-if [[ ! -e $PYENV ]]; then
+if [[ ! -d "$PYENV" ]]; then
+    echo "creating python environment - $PYENV"
     python3 -m venv venv
-elif [[ ! -d $PYENV ]]; then
-    echo "$PYENV already exists but is not a directory"
+else; then
+    echo "$PYENV already exists. Using as virtual environment"
 fi
 source venv/bin/activate
 pip install -r requirements.txt
