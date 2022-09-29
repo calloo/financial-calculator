cb4c8e6df03161c3260df1f2a0f912faaf4b9f64
Michael Calloo <admin@cookienerds.com>
2022-09-29 17:34:28 +0800

    fix sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,4 @@
+apt-get --yes install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0
+python3 -m venv venv
 source venv/bin/activate
-sudo apt install python3-gi python3-gi-cairo gir1.2-gtk-3.0 gir1.2-webkit2-4.0
 pip install -r requirements.txt
